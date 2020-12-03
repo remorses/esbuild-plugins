e4ec87b937e41bfcb4580bf46c4df2a76fc964d1
remorses <beats.by.morse@gmail.com>
2020-12-03 11:38:38 +0100

    added node-resolve plugin

---
+++ node-resolve/src/index.ts
@@ -0,0 +1,53 @@
+import { build, OnResolveArgs, Plugin } from 'esbuild'
+import path from 'path'
+import fs from 'fs'
+import builtins from 'builtin-modules'
+import resolve from 'resolve'
+
+const NAMESPACE = 'node-resolve'
+
+export function NodeResolvePlugin(): Plugin {
+    const builtinsSet = new Set(builtins)
+    return {
+        name: 'custom-resolver',
+        setup: function setup({ onLoad, onResolve }) {
+            onLoad({ filter: /.*/, namespace: NAMESPACE }, async (args) => {
+                const contents = await (
+                    await fs.promises.readFile(args.path)
+                ).toString()
+                let resolveDir = path.dirname(args.path)
+                // console.log({ resolveDir })
+                return {
+                    loader: 'js',
+                    contents,
+                    resolveDir,
+                    // errors: [{ text: resolveDir }],
+                }
+            })
+            
+            
+            onResolve({ filter: /.*/ }, function resolver(args: OnResolveArgs)  {
+                if (builtinsSet.has(args.path)) {
+                    return null
+                }
+                const resolved = resolve.sync(args.path, {
+                    basedir: args.resolveDir,
+                    extensions: ['.ts', '.tsx', '.mjs', '.js', '.jsx',  '.cjs'],
+                })
+
+                if (!resolved) {
+                    return {
+                        external: true,
+                        errors: [{ text: 'could not resolve ' + args.path }],
+                    }
+                }
+                return {
+                    path: resolved,
+                    namespace: NAMESPACE,
+                }
+            })
+
+            // onResolve({ filter: /.*/, namespace: NAMESPACE }, resolver)
+        },
+    }
+}
