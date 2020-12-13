aaacf887ff8908336017abd145d8d78a48eeb345
remorses <beats.by.morse@gmail.com>
2020-12-13 19:38:41 +0100

    added onResolve option

--- node-resolve/src/index.ts
+++ node-resolve/src/index.ts
@@ -17,12 +17,14 @@ const resolveAsync: (
 interface Options {
     external?: (path: string) => boolean | OnResolveResult | undefined
     onUnresolved?: (e: Error) => OnResolveResult | undefined | null | void
+    onResolved?: (p: string) => Promise<any> | any
     resolveOptions?: Partial<AsyncOpts>
 }
 
 export function NodeResolvePlugin({
     external,
     onUnresolved,
+    onResolved,
     resolveOptions,
 }: Options = {}): Plugin {
     const builtinsSet = new Set(builtins)
@@ -31,70 +33,91 @@ export function NodeResolvePlugin({
         name: NAME,
         setup: function setup({ onLoad, onResolve }) {
             onLoad({ filter: /.*/, namespace: NAMESPACE }, async (args) => {
-                const contents = await (
-                    await fs.promises.readFile(args.path, { encoding: 'utf-8' })
-                ).toString()
-                let resolveDir = path.dirname(args.path)
-                // console.log({ resolveDir })
-                debug('onLoad')
-                return {
-                    loader: 'default',
-                    contents,
-                    resolveDir,
-                    // errors: [{ text: resolveDir }],
+                try {
+                    const contents = await (
+                        await fs.promises.readFile(args.path, {
+                            encoding: 'utf-8',
+                        })
+                    ).toString()
+                    let resolveDir = path.dirname(args.path)
+                    // console.log({ resolveDir })
+                    debug('onLoad')
+                    return {
+                        loader: 'default',
+                        contents,
+                        resolveDir,
+                        // errors: [{ text: resolveDir }],
+                    }
+                } catch (e) {
+                    throw new Error(`Cannot load ${args.path}, ${e}`)
                 }
             })
 
-            onResolve({ filter: /.*/ }, async function resolver(
-                args: OnResolveArgs,
-            ) {
-                if (builtinsSet.has(args.path)) {
-                    return null
-                }
-                let resolved
-                try {
-                    resolved = await resolveAsync(args.path, {
-                        basedir: args.resolveDir,
-                        extensions: [
-                            '.ts',
-                            '.tsx',
-                            '.mjs',
-                            '.js',
-                            '.jsx',
-                            '.cjs',
-                        ],
-                        ...resolveOptions,
-                    })
-                } catch (e) {
-                    if (onUnresolved) {
-                        debug(`not resolved ${args.path}`)
-                        let res = onUnresolved(e)
-                        return res || null
-                    } else {
-                        throw e
+            onResolve(
+                { filter: /.*/ },
+                async function resolver(args: OnResolveArgs) {
+                    if (builtinsSet.has(args.path)) {
+                        return null
                     }
-                }
-                debug('resolved', resolved)
-                if (external) {
-                    debug('externalizing', external)
-                    const res = external(resolved)
-                    if (res === true) {
-                        return {
-                            path: resolved,
-                            external: true, // TODO maybe use the ESM external trick?
+                    let resolved
+                    try {
+                        resolved = await resolveAsync(args.path, {
+                            basedir: args.resolveDir,
+                            extensions: [
+                                '.ts',
+                                '.tsx',
+                                '.mjs',
+                                '.js',
+                                '.jsx',
+                                '.cjs',
+                            ],
+                            ...resolveOptions,
+                        })
+                    } catch (e) {
+                        if (onUnresolved) {
+                            debug(`not resolved ${args.path}`)
+                            let res = onUnresolved(e)
+                            return res || null
+                        } else {
+                            throw e
                         }
                     }
-                    if (res) {
-                        return res
+                    debug('resolved', resolved)
+                    if (resolved && onResolved) {
+                        const res = await onResolved(resolved)
+                        if (typeof res === 'string') {
+                            return {
+                                path: res,
+                                namespace: NAMESPACE,
+                            }
+                        }
+                        if (res?.path) {
+                            return res
+                        }
                     }
-                }
+                    
+                    // TODO remove external, external can be expressed with onResolved
+                    if (external) {
+                        debug('externalizing', external)
+                        const res = external(resolved)
+                        if (res === true) {
+                            return {
+                                path: resolved,
+                                external: true, // TODO maybe use the ESM external trick?
+                            }
+                        }
+                        if (res) {
+                            return res
+                        }
+                    }
 
-                debug('onResolve')
-                return {
-                    path: resolved,
-                    namespace: NAMESPACE,
-                }
-            })
+                    debug('onResolve')
+                    return {
+                        path: resolved,
+                        namespace: NAMESPACE,
+                    }
+                },
+            )
         },
     }
 }
