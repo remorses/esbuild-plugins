cb4dceff68048d4f7d26b260eaa1cc92514526d7
remorses <beats.by.morse@gmail.com>
2021-01-11 16:28:37 +0100

    added buffer injection

--- node-globals-polyfill/src/index.test.ts
+++ node-globals-polyfill/src/index.test.ts
@@ -3,7 +3,7 @@ import { writeFiles } from 'test-support'
 
 require('debug').enable(require('../package.json').name)
 
-test('works', async () => {
+test('process works', async () => {
     const {
         unlink,
         paths: [ENTRY],
@@ -21,5 +21,66 @@ test('works', async () => {
     const output = res.outputFiles[0].text
     // console.log(output)
     eval(output)
+    unlink()
+})
+
+test('process is tree shaken', async () => {
+    const {
+        unlink,
+        paths: [ENTRY],
+    } = await writeFiles({
+        'entry.ts': `console.log('hei')`,
+    })
+    const res = await build({
+        entryPoints: [ENTRY],
+        write: false,
+        format: 'esm',
+        target: 'es2017',
+        bundle: true,
+        inject: [require.resolve('../process')],
+    })
+    const output = res.outputFiles[0].text
+    expect(output.length).not.toContain('process')
+    unlink()
+})
+
+test('Buffer works', async () => {
+    const {
+        unlink,
+        paths: [ENTRY],
+    } = await writeFiles({
+        'entry.ts': `new Buffer('xxx')`,
+    })
+    const res = await build({
+        entryPoints: [ENTRY],
+        write: false,
+        format: 'esm',
+        target: 'es2017',
+        bundle: true,
+        inject: [require.resolve('../process')],
+    })
+    const output = res.outputFiles[0].text
+    // console.log(output)
+    eval(output)
+    unlink()
+})
+
+test('Buffer is tree shaken', async () => {
+    const {
+        unlink,
+        paths: [ENTRY],
+    } = await writeFiles({
+        'entry.ts': `console.log('hei')`,
+    })
+    const res = await build({
+        entryPoints: [ENTRY],
+        write: false,
+        format: 'esm',
+        target: 'es2017',
+        bundle: true,
+        inject: [require.resolve('../process')],
+    })
+    const output = res.outputFiles[0].text
+    expect(output.length).not.toContain('Buffer')
     unlink()
 })
